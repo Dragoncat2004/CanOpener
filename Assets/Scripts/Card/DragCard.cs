1d95bb9e5f7bbc31b7905648fc9803e892a9a754
FourD <minseongkim2004@gmail.com>
2026-04-21 15:27:01 +0900

    드래그&드랍수정본
    드래그&드랍수정본

--- Assets/Scripts/Card/DragCard.cs
+++ Assets/Scripts/Card/DragCard.cs
@@ -6,11 +6,7 @@ using UnityEngine.UI;
 public class DragCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private Transform dragCanvas;
-
-    // 드래그할 때 마우스를 따라다닐 '가짜 복제본'을 담을 변수
     private GameObject dragGhost;
-
-    // 내 카드의 정보 (나중에 덱에 넘겨주기 위해 필요함)
     public CardData myCardData;
 
     void Awake()
@@ -18,46 +14,47 @@ public class DragCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         GameObject canvasObj = GameObject.Find("DragCanvas");
         if (canvasObj != null) dragCanvas = canvasObj.transform;
 
-        // 만약 CardDisplay 스크립트가 붙어있다면 데이터를 자동으로 가져옴
         if (TryGetComponent(out CardDisplay display))
         {
             myCardData = display.cardData;
         }
     }
 
+    // ⭐ 2. 매 프레임마다 우클릭을 감시합니다.
+    void Update()
+    {
+
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        // 1. 원본(나 자신)은 가만히 두고, 나와 똑같이 생긴 '가짜'를 생성합니다.
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("<color=yellow>우클릭: 드래그 시작이 취소되었습니다.</color>");
+            return; // 우클릭으로 드래그 시작 자체를 막습니다.
+        }
+
         dragGhost = Instantiate(this.gameObject, dragCanvas);
 
-        // ⭐ [핵심 해결책] 원본 카드의 실제 크기를 유령에게 똑같이 적용합니다! ⭐
         RectTransform originalRect = GetComponent<RectTransform>();
         RectTransform ghostRect = dragGhost.GetComponent<RectTransform>();
-
-        // Grid Layout이 만들어준 원본의 폭(width)과 높(height)을 그대로 복사
         ghostRect.sizeDelta = new Vector2(originalRect.rect.width, originalRect.rect.height);
 
-        // (선택) 만약 카드가 마우스 포인터 정중앙에 안 온다면 pivot도 맞춰줍니다.
-        ghostRect.pivot = originalRect.pivot;
-
-        // 2. 가짜 카드는 드래그 용도이므로 광선(Raycast)을 막지 않게 설정합니다.
-        // (그래야 가짜 카드 너머에 있는 덱 슬롯이 마우스를 인식할 수 있음)
         if (dragGhost.TryGetComponent(out CanvasGroup ghostGroup))
         {
             ghostGroup.blocksRaycasts = false;
-            //ghostGroup.alpha = 0.8f; // 약간 투명하게 해서 드래그 중임을 표현
+            ghostGroup.alpha = 0.8f;
         }
-        // 가짜 카드에 붙어있는 CardDisplay를 찾아서, 내 데이터를 다시 주입해줍니다.
+
         if (dragGhost.TryGetComponent(out CardDisplay ghostDisplay))
         {
             ghostDisplay.Setup(myCardData);
-            Destroy(dragGhost.GetComponent<DragCard>());
         }
+        Destroy(dragGhost.GetComponent<DragCard>());
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        // 3. 원본 대신 '가짜'가 마우스를 따라다니게 합니다.
         if (dragGhost != null)
         {
             dragGhost.transform.position = eventData.position;
@@ -66,10 +63,12 @@ public class DragCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        // 4. 마우스에서 손을 떼면 임무를 다한 가짜는 삭제합니다.
+        // 4. OnEndDrag가 정상 발동되더라도, 유령 카드가 남아있을 때만 파괴합니다.
+        // (우클릭으로 이미 파괴했다면 여기는 안전하게 패스됩니다.)
         if (dragGhost != null)
         {
             Destroy(dragGhost);
+            dragGhost = null;
         }
     }
 }
--- Assets/Scripts/UI/DeckDropZone.cs
+++ Assets/Scripts/UI/DeckDropZone.cs
@@ -3,27 +3,31 @@ using UnityEngine.EventSystems;
 
 public class DeckDropZone : MonoBehaviour, IDropHandler
 {
-    public GameObject cardPrefab; // 덱에 생성할 카드 프리팹
+    public GameObject cardPrefab;
 
     public void OnDrop(PointerEventData eventData)
     {
-        // eventData.pointerDrag에는 항상 '처음 클릭했던 원본'이 들어있습니다.
         if (eventData.pointerDrag != null)
         {
             if (eventData.pointerDrag.TryGetComponent(out DragCard originalCard))
             {
-                Debug.Log($"<color=cyan>[덱 편집기]</color> '{originalCard.myCardData.cardName}' 카드를 덱에 추가합니다!");
-
-                // 1. 덱 슬롯(내 아래)에 새로운 진짜 카드를 생성합니다.
-                GameObject newCardInDeck = Instantiate(cardPrefab, this.transform);
-
-                // 2. 원본 카드가 가지고 있던 데이터를 새 카드에 주입합니다.
-                if (newCardInDeck.TryGetComponent(out CardDisplay display))
+                if (originalCard.myCardData == null)
                 {
-                    display.Setup(originalCard.myCardData);
+                    Debug.LogWarning($"<color=yellow>[덱 편집기]</color> 드래그된 카드에 카드 데이터가 없습니다!");
+                    return;
+                }   
+
+                CardData droppedData = originalCard.myCardData;
+                if (droppedData != null)
+                {
+                    Debug.Log($"<color=cyan>[덱 편집기]</color> '{droppedData.cardName}' 카드를 덱에 추가합니다!");
                 }
 
-                // 3. (선택) 여기에 DeckManager.AddCard(originalCard.myCardData) 같은 로직 추가
+                    GameObject newCardInDeck = Instantiate(cardPrefab, this.transform);
+                if (newCardInDeck.TryGetComponent(out CardDisplay display))
+                {
+                    display.Setup(droppedData);
+                }
             }
         }
     }
